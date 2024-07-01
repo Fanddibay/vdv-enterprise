80b4b013830edfb60cbf7c492311f4f534a21114
fanbayy <fandi.bayu110@gmail.com>
2024-07-01 16:31:59 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "background-img": "#F9F9F9",
+        "text-color": "#252525",
+        "blue-custom": "#39A8ED",
+      },
+    },
+  },
+  plugins: [],
+};
